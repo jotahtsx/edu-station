48fe85fe0b70ad20ebf33784b0a0826d3de735f4
João Manoel <joaomanoelskai@gmail.com>
2024-05-01 07:35:28 -0300

    feat inicializando rows

--- resources/views/admin/master/master.blade.php
+++ resources/views/admin/master/master.blade.php
@@ -21,14 +21,17 @@
 
 <body>
 
-    <p class="edu-h1">lorem ipsum dolor sit amet, consectet</p>
+    <div class="edu-container">
 
-    <h1>Heading 1</h1>
-    <h2>Heading 2</h2>
-    <h3>Heading 3</h3>
-    <h4>Heading 4</h4>
-    <h5>Heading 5</h5>
-    <h6>Heading 6</h6>
+        <p class="edu-h1">lorem ipsum dolor sit amet, consectet</p>
+
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <h4>Heading 4</h4>
+        <h5>Heading 5</h5>
+        <h6>Heading 6</h6>
+    </div>
 
     <script src="{{ url(mix('backend/assets/js/jquery.js')) }}"></script>
     <script src="{{ url(mix('backend/assets/js/app.js')) }}"></script>
