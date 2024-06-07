ad11f58ffc26c2ddb2e7842a9d6b9036306627d9
João Manoel <joaomanoelskai@gmail.com>
2024-06-07 05:39:23 -0300

    feat: inicializando sidebar

--- resources/views/admin/master/master.blade.php
+++ resources/views/admin/master/master.blade.php
@@ -42,13 +42,53 @@
                         </div>
                     </div>
 
-                    <ul class="edu-header__navitems edu-header__sidebar-md-3 edu-header__sidebar-lg-2 edu-pd-y-20"
-                        id="main-menu">
+                    <ul class="edu-header__navitems edu-header__sidebar-md-3 edu-header__sidebar-lg-2" id="main-menu">
 
                         <li class="edu-header__list">
                             <a href="{{ route('admin.home') }}"
                                 class="edu-header__link--logo edu-pd-20 edu-d-flex edu-align-center edu-justify-center">
                                 <img src="{{ url(asset('backend/assets/images/logo.svg')) }}" />
+                            </a>
+                        </li>
+
+                        <li class="edu-header__list">
+                            <a href="direcao"
+                                class="edu-header__link edu-pd-20 edu-d-flex edu-align-center edu-justify-between">
+                                <span class="edu-fs-14 edu-d-flex edu-align-center">
+                                    <i class="ri-message-2-line edu-mg-r-10"></i>Direção
+                                </span>
+                            </a>
+                        </li>
+                        <li class="edu-header__list">
+                            <a href="docente"
+                                class="edu-header__link edu-pd-20 edu-d-flex edu-align-center edu-justify-between">
+                                <span class="edu-fs-14 edu-d-flex edu-align-center">
+                                    <i class="ri-message-2-line edu-mg-r-10"></i>Docente
+                                </span>
+                            </a>
+                        </li>
+                        <li class="edu-header__list">
+                            <a href="docente"
+                                class="edu-header__link edu-pd-20 edu-d-flex edu-align-center edu-justify-between">
+                                <span class="edu-fs-14 edu-d-flex edu-align-center">
+                                    <i class="ri-message-2-line edu-mg-r-10"></i>Aluno(a)
+                                </span>
+                            </a>
+                        </li>
+                        <li class="edu-header__list">
+                            <a href="disciplina"
+                                class="edu-header__link edu-pd-20 edu-d-flex edu-align-center edu-justify-between">
+                                <span class="edu-fs-14 edu-d-flex edu-align-center">
+                                    <i class="ri-message-2-line edu-mg-r-10"></i>Disciplina
+                                </span>
+                            </a>
+                        </li>
+                        <li class="edu-header__list">
+                            <a href="turma"
+                                class="edu-header__link edu-pd-20 edu-d-flex edu-align-center edu-justify-between">
+                                <span class="edu-fs-14 edu-d-flex edu-align-center">
+                                    <i class="ri-message-2-line edu-mg-r-10"></i>Turma
+                                </span>
                             </a>
                         </li>
                     </ul>
