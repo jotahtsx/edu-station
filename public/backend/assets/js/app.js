d9ea6dea82d0734414cdcd797cfc1a0c8af3e0af
João Manoel <joaomanoelskai@gmail.com>
2024-06-12 01:20:07 -0300

    test: testando o comportamento dos dropdowns e suas respectivas classes[3]

--- public/backend/assets/js/app.js
+++ public/backend/assets/js/app.js
@@ -4,6 +4,7 @@ const dropdown = () => {
     menu: "edu-dropdown__menu",
     target: "data-dropdown-target",
     active: "edu-active",
+    components: [...document.querySelectorAll(`.edu-dropdown`)]
   }
 
   const dropDown = [...document.querySelectorAll(`.${_variables.main}`)]
@@ -20,7 +21,7 @@ const dropdown = () => {
 
     const activeMenu = document.querySelector(`#${targetId}`)
 
-    const nonTargeted = dropDown.map(drop => {
+    const nonTargeted = _variables.components.map(drop => {
       const nonActiveId = drop.querySelector(`[${_variables.target}]`).getAttribute(_variables.target)
       const nonActive = document.querySelector(`#${nonActiveId}`)
 
@@ -33,6 +34,19 @@ const dropdown = () => {
 
     if (activeMenu) activeMenu.classList.toggle(_variables.active)
 
+  })
+
+  window.addEventListener("mouseup", edu => {
+
+    const target = edu.target.closest(`[${_variables.target}]`) || edu.target.closest(`.${_variables.target}`)
+    console.log(target)
+
+    if(target) return
+
+    _variables.components.forEach( drop => {
+      const menu = drop.querySelector(`.${_variables.menu}`)
+      if(menu.classList.contains(_variables.active)) menu.classList.remove(_variables.active)
+    })
   })
 
 }
@@ -49,10 +63,42 @@ const navbar = () => {
     components: [...document.querySelectorAll(".edu-header")]
   }
 
-  document.addEventListener("click", edu=> {
+  document.addEventListener("click", edu => {
     const targetBtn = edu.target.closest(`.${_variables.target}`);
-    console.log(targetBtn)
+    if (!targetBtn) return
+
+    edu.preventDefault()
+
+    const targetId = targetBtn.getAttribute(_variables.targetId)
+    const activeMenu = document.querySelector(`#${targetId}`)
+
+    const nonTargeted = _variables.components.map(head => {
+      const nonActiveId = head.querySelector(`[${_variables.targetId}]`).getAttribute(_variables.targetId)
+      const nonActive = document.querySelector(`#${nonActiveId}`)
+
+      return nonActive
+    })
+
+    const filterExceptActive = nonTargeted.filter(target => target !== activeMenu)
+
+    filterExceptActive.forEach(head => head.classList.remove(_variables.active))
+
+    if (activeMenu) activeMenu.classList.toggle(_variables.active)
   })
+
+  window.addEventListener("mouseup", edu => {
+
+    const target = edu.target.closest(`.${_variables.menu}`) || edu.target.closest(`.${_variables.target}`)
+    console.log(target)
+
+    if(target) return
+
+    _variables.components.forEach(head => {
+      const menu = head.querySelector(`.${_variables.menu}`)
+      if(menu.classList.contains(_variables.active)) menu.classList.remove(_variables.active)
+    })
+  })
+
 }
 
 navbar()
