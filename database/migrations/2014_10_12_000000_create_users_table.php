86acf3d2eecfd87b6a9fc1697a3454cd4671743e
João Manoel <joaomanoelskai@gmail.com>
2024-06-23 13:17:05 -0300

    fix: retorno para tela de login e ip de usuário logado e último login ip

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('last_login_ip')->nullable();
+            $table->dateTime('last_login_at')->nullable();
             $table->timestamps();
         });
     }
